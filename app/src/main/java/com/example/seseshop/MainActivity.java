6c2d4bfc3f2a9e0249434c0a97737d5853243528
MikkelFiltenborg <mikkelsfiltenborg@gmail.com>
2024-09-06 10:10:03 +0200

    Created Adapter Class and implemented it in MainActivity.

--- app/src/main/java/com/example/seseshop/MainActivity.java
+++ app/src/main/java/com/example/seseshop/MainActivity.java
@@ -7,18 +7,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) ->
+        {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        List<String> magicalItemList = Arrays.asList(
+                "MagicalItem 1",
+                "MagicalItem 2",
+                "MagicalItem 3"
+        );
+
+        RecyclerView recyclerView = findViewById(R.id.item_list_view);
+        MagicalItemAdapter listAdapter = new MagicalItemAdapter(this, magicalItemList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(listAdapter);
     }
 }
