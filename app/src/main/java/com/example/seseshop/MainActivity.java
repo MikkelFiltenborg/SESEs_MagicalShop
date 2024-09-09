1c6d8fc760e7e0f3b97f5ef582f71bee99bb66be
MikkelFiltenborg <mikkelsfiltenborg@gmail.com>
2024-09-09 18:29:38 +0200

    Placeholder image used to substitude in case opf lacking item image in object. Main menu list updated to show relevant attributes of items in list. Info page changed to dynamically change content depending on selected item from list. placeholder_img drawable added, used with dynamic sizing in xml.

--- app/src/main/java/com/example/seseshop/MainActivity.java
+++ app/src/main/java/com/example/seseshop/MainActivity.java
@@ -13,8 +13,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.seseshop.adapters.MagicalItemWaresAdapter;
+import com.example.seseshop.models.MagicItem;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
@@ -32,11 +33,26 @@ public class MainActivity extends AppCompatActivity
             return insets;
         });
 
-        List<String> magicalItemList = Arrays.asList(
-                "MagicalItemName 1",
-                "MagicalItemName 2",
-                "MagicalItemName 3"
-        );
+        List<MagicItem> magicalItemList = new ArrayList<>();
+        magicalItemList.add(new MagicItem(
+                1,
+                1,
+                3872.00,
+                "The Wizards Bane",
+                "A mythical greatsword, forged from Adamantium and imbued with a powerful field of anti magic that surrounds its wielder.",
+                "images/TWB-LS.png"));
+        magicalItemList.add(new MagicItem(2,
+                6,
+                285.25,
+                "Bag of Holding",
+                "A special bad that looks like a shoulder satchel, decirated with a face. The bag is capable of storing multiple items, despite it's small size",
+                "images/BoH-EQ.png"));
+        magicalItemList.add(new MagicItem(3,
+                2,
+                762.50,
+                "Bomarang Daggar",
+                "This nifty little bladed weapon can be used as a dual sided dagger or throws, upon which it will fly out 50ft and then return to it's user in an elyptical arch.",
+                "images/BD-TW.png"));
 
         RecyclerView recyclerView = findViewById(R.id.item_list_view);
         MagicalItemWaresAdapter listAdapter =
@@ -44,7 +60,7 @@ public class MainActivity extends AppCompatActivity
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(listAdapter);
 
-        ImageView viewBasketBtn = findViewById(R.id.basket_imgbtn);
+        ImageView viewBasketBtn = findViewById(R.id.basket_iconbtn);
         viewBasketBtn.setOnClickListener(view ->
         {
             Intent intent = new Intent(MainActivity.this, BasketActivity.class);
